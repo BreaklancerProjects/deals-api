6ffee325459e25b25761cdfb3b6639a2a733af32
Jesús Miguel Echevarría Díaz <jmechevarria2015@gmail.com>
2024-07-09 09:07:29 +0200

    log files

--- src/api-functions/api.ts
+++ src/api-functions/api.ts
@@ -1,5 +1,60 @@
 console.log(`Executing code in api.ts`);
 import serverless from 'serverless-http';
 import app from '../server';
+import fs from 'fs';
+import path from 'path';
+
+console.log('files in', path.join(process.cwd()));
+fs.promises
+  .readdir(path.join(process.cwd()))
+
+  // If promise resolved and
+  // data are fetched
+  .then((filenames) => {
+    for (const filename of filenames) {
+      console.log(filename);
+    }
+  })
+
+  // If promise is rejected
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    console.log('files in', path.join(process.cwd(), 'src'));
+    fs.promises
+      .readdir(path.join(process.cwd(), 'src'))
+
+      // If promise resolved and
+      // data are fetched
+      .then((filenames) => {
+        for (const filename of filenames) {
+          console.log(filename);
+        }
+      })
+
+      // If promise is rejected
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        console.log('files in', path.join(process.cwd(), 'src', 'api-functions'));
+        fs.promises
+          .readdir(path.join(process.cwd(), 'src', 'api-functions'))
+
+          // If promise resolved and
+          // data are fetched
+          .then((filenames) => {
+            for (const filename of filenames) {
+              console.log(filename);
+            }
+          })
+
+          // If promise is rejected
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+  });
 
 export const handler = serverless(app);
